1c5b46e0ef803e4915bda1d9e97693e2df50ef4f
Yi-Hua <yivavalu@gmail.com>
2024-07-03 10:30:12 +0800

    add(menu): add menu model

---
+++ angular-app/src/app/pages/menu.model.ts
@@ -0,0 +1,14 @@
+export const menu = [
+  {
+    id: 1,
+    key: 'home',
+    href: '/pages/home',
+    matIcon: 'home'
+  },
+  {
+    id: 2,
+    key: 'create',
+    href: '/pages/create',
+    matIcon: 'add'
+  },
+]
--- angular-app/src/app/pages/pages.component.ts
+++ angular-app/src/app/pages/pages.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module'
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { TranslateService } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
+import { menu } from './menu.model';
 
 @Component({
   selector: 'app-pages',
   standalone: true,
-  imports: [ RouterOutlet, MatToolbarModule, MatSidenavModule, SharedModule],
+  imports: [ RouterOutlet, RouterModule, MatToolbarModule, MatSidenavModule, SharedModule],
   templateUrl: './pages.component.html',
   styleUrl: './pages.component.scss'
 })
 
 export class PagesComponent implements OnInit{
   isNavOpened = false;
+  menu = menu;
 
   constructor(private translate: TranslateService, private http: HttpClient) {}
 
@@ -24,7 +27,5 @@ export class PagesComponent implements OnInit{
 
   toggleMenu(): void {
     this.isNavOpened = !this.isNavOpened
-    console.log('Menu!')
   }
-
 }
